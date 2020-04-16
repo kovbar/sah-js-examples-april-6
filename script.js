9dfff84eaa1758991f479c0d455f5379154bcc07
kovbar <63658107+kovbar@users.noreply.github.com>
2020-04-16 19:35:49 +0200

    fourth part

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ $('main').append('<p id="second">Added second element</p>')
 $('#first').remove();
 
 $('#add-button').click(() => {
+   console.log($('main p'));
 $('main').append('<p>Added a new p tag</p>')
 
 });
@@ -36,7 +37,66 @@ let removeFunction = ()=>{
 $('#remove-button').click(removeFunction);
 
 
+/* last part
+conditions and loops */
 
+let age = 35   ;
+
+if ( age > 18 ){
+    console.log('This person can drink.');
+    /* statements 
+    !- not equal!
+    nem lesz egyenlő ha felkiáltójel van!*/
+}else{
+console.log('this is a kid, no alcohol.');
+}
+
+
+/* loops!!!!
+used for repeatedly do the same time, just like for 100th times, it can be hard sometimes
+first is the most useful
+ */
+
+
+
+/* $('main').append('<div class="box"></div>');
+$('main div:last-child').css('background', 'orange')
+$('main').append('<div class="box"></div>');
+$('main div:last-child').css('background', 'yellow')
+$('main').append('<div class="box"></div>');
+$('main div:last-child').css('background', 'purple')
+$('main').append('<div class="box"></div>');
+$('main div:last-child').css('background', 'blue')
+$('main').append('<div class="box"></div>');
+$('main div:last-child').css('background', 'green')
+ */
+let colors = ['orange', 'yellow', 'purple', 'blue', 'green'];  
+colors.push('magenta');
+
+ colors.forEach((color)=>{
+    $('main').append('<div class="box"></div>');
+    $('main div:last-child').css('background', color)
+ });
+
+
+
+
+
+
+for(let i = 0; i < 100; i = i + 1 ) {
+    console.log('Current number' + i);
+}
+
+
+
+
+
+
+
+
+
+
+ 
 /* 123
 123
 5+5
